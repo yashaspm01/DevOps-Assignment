4647760aa7d74b33eca2b2a38dbe6e57be908684
Yashas <yashaspm01@gmail.com>
2026-02-28 08:58:46 +0530

    Terraform Accepted

--- DevOps-Assignment-Terraform/modules/security-groups/main.tf
+++ DevOps-Assignment-Terraform/modules/security-groups/main.tf
@@ -15,6 +15,27 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["49.204.123.10/32"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["49.204.123.10/32"]
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
